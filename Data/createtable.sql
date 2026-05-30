7cc22b8af648818d0aa878b6368f310c53822fec
May <zhouyuetong@Mays-MacBook-Pro-2.local>
2026-05-29 19:39:16 -0500

    Change sql

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -2,15 +2,15 @@ DROP TABLE IF EXISTS llm_energy;
 CREATE TABLE llm_energy (
     id SERIAL PRIMARY KEY,
     model_name VARCHAR(100) NOT NULL,
-    model_parameters_billion FLOAT,
-    training_tokens_billion FLOAT,
+    model_parameters_billion VARCHAR(50),
+    training_tokens_billion VARCHAR(50),
     gpu_type VARCHAR(50),
-    num_gpus INT,
-    training_hours FLOAT,
+    num_gpus VARCHAR(20),
+    training_hours VARCHAR(20),
     data_center_region VARCHAR(100),
-    pue FLOAT,
-    hardware_power_draw_watts_per_gpu INT,
-    carbon_intensity_gco2_per_kwh INT,
-    total_energy_kwh FLOAT,
-    total_carbon_footprint_kgco2e FLOAT
+    pue VARCHAR(20),
+    hardware_power_draw_watts_per_gpu VARCHAR(20),
+    carbon_intensity_gco2_per_kwh VARCHAR(20),
+    total_energy_kwh VARCHAR(20),
+    total_carbon_footprint_kgco2e VARCHAR(20)
 );
